1aad4fd6690132aff7441e810998db464189310f
belkixrequejo <belkix.requejo@pucese.edu.ec>
2020-06-15 07:17:08 -0500

    Add files config

---
+++ src/config/dbConnection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+module.exports = () => {
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'usuario',
+    password: 'contraseña',
+    database: 'nombre de la base de datos',
+    port: ''
+  });
+}
